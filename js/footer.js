5f2e323894d40953035e663aa4d9c81ddb711e9e
vanshgupta7 <vansh.vanshgupta@gmail.com>
2022-06-22 02:14:49 +0530

    first commit

---
+++ js/footer.js
@@ -0,0 +1,39 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+    footer.innerHTML=`
+    <div class="content">
+            <div class="content-1">
+                <h1 class="content-header">Team Anant</h1><br>
+                <h2 class="content-subheader">BITS Pilani</h3><br>
+                <a href="mailto:anant_coordinator@pilani.bits-pilani.ac.in" target="_blank" 
+                    class="footer-email">
+                    <i class="fa-regular fa-envelope"></i>
+                    anant_coordinator@pilani.bits-pilani.ac.in
+                </a>
+            </div>
+            <!-- <div class="content-2">
+                <h1>Contact Details</h1><br>
+                <div class="phone-number">
+                    <i class="fa-solid fa-phone"></i><br>
+                </div>
+                <div class="email">
+                    <i class="fa-regular fa-envelope"></i>
+                    <p></p>
+                </div>
+            </div> -->
+            <div class="content-3">
+                <h1 class="content-header">Our Pages</h1><br>
+                <div class="content-3links">
+                    <a href="https://m.facebook.com/219800068200346/"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="https://instagram.com/teamanantbits?utm_medium=copy_link">
+                        <i class="fa-brands fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/company/teamanant-bits">
+                        <i class="fa-brands fa-linkedin"></i></a>
+                    <a href="https://github.com/team-anant"><i class="fa-brands fa-github"></i></a>
+                </div>
+            </div>
+    </div>
+    `
+}
+
+createFooter();
---
+++ js/navbar.js
@@ -0,0 +1,46 @@
+// const hamburger = document.querySelector(".hamburger");  
+// const navsub = document.querySelector(".nav-items");  
+// hamburger.addEventListener('click', () => {  
+//  hamburger.classList.toggle("change")  
+//  navsub.classList.toggle("nav-change")  
+// }); 
+
+
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+    nav.innerHTML=`
+    <div class="nav">
+    <img src="images1/a.png" class="brand-logo" alt="">
+    <div class="nav-items">
+        <li><a href = "index.html">Home</a></li>
+        <li><a href = "updates.html">Updates</a></li>
+        <li><a href = "papers.html">Publications</a></li>
+        <li class = "dropdown">
+            <a class = "dropdown-toggle" data-toggle = "dropdown" href = "subsystems.html">Subsystems
+                <i class="fa-solid fa-angle-down"></i>
+            </a>
+            <ul class = "dropdown-menu">
+            <li><a href = "subsystems.html#pal">PAYLOAD</a></li>
+            <li><a href = "subsystems.html#obc">OBC</a></li>
+            <li><a href = "subsystems.html#eps">EPS</a></li>
+            <li><a href = "subsystems.html#adcs">ADCS</a></li>
+            <li><a href = "subsystems.html#mech">MECH</a></li>
+            <li><a href = "subsystems.html#tele">TELE</a></li>
+            <li><a href = "subsystems.html#psd">PSD</a></li>
+            </ul>
+        </li> 
+        <li><a href = "team.html">Team</a></li>
+        <li><a href = "sponsor.html">Sponsor</a></li>
+        <li><a href = "contact.html">Contact Us</a></li>
+    </div>
+    <div class="hamburger">
+        <span class="icon-bar" id="bar1"></span>
+        <span class="icon-bar" id="bar2"></span>
+        <span class="icon-bar" id="bar3"></span>
+    </div>
+   
+    </div>
+    `
+}
+
+createNav();
